7d26570fba83d845c8330d57b7e0419a5bee9536
alisagudo <algudo@taltech.ee>
2025-11-01 18:00:44 +0200

    Prettier + ESLint seadistatud

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,43 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import pluginReact from "eslint-plugin-react"
+import pluginPrettier from "eslint-plugin-prettier"
+import configPrettier from "eslint-config-prettier"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
-]);
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      prettier: pluginPrettier,
+    },
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      configPrettier,
+    ],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 100,
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+])
