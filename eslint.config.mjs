ba09ce04ecae52d9debcead55cfb3842fa918b8a
alisagudo <algudo@taltech.ee>
2025-11-08 15:09:38 +0200

    Täiendasin ESLint konfiguratsiooni -  lisasin Jest testikeskkonna toe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,18 @@
-import js from "@eslint/js"
-import globals from "globals"
-import pluginReact from "eslint-plugin-react"
-import pluginPrettier from "eslint-plugin-prettier"
-import configPrettier from "eslint-config-prettier"
-import { defineConfig } from "eslint/config"
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"], 
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
+        ...globals.jest, 
       },
     },
     plugins: {
