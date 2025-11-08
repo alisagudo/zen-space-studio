abc5b87096617fd425454a9a1f2b1562ce98cd3a
alisagudo <algudo@taltech.ee>
2025-11-08 15:12:27 +0200

    Täiendasin ESLint konfiguratsiooni - lisasin Jest testikeskkonna toe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import pluginPrettier from "eslint-plugin-prettier";
-import configPrettier from "eslint-config-prettier";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"], 
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
-        ...globals.jest, 
+        ...globals.jest,
       },
     },
     plugins: {
       react: pluginReact,
       prettier: pluginPrettier,
     },
-    extends: [
-      js.configs.recommended,
-      pluginReact.configs.flat.recommended,
-      configPrettier,
-    ],
+    extends: [js.configs.recommended, pluginReact.configs.flat.recommended, configPrettier],
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "prettier/prettier": [
-        "error",
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': [
+        'error',
         {
           singleQuote: true,
           semi: false,
@@ -37,7 +33,7 @@ export default defineConfig([
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
