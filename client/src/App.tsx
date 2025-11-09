e10334388773b55280b6ad5d1404ff22f6656217
alisagudo <algudo@taltech.ee>
2025-11-09 02:16:57 +0200

    "Teenused" vaheleht lisatud

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Navigation } from './components/Navbar'
 import Avaleht from './pages/Avaleht'
 import { Booking } from './pages/Booking'
-import Teenused from './pages/Teenused'
+import { Services } from './pages/Teenused'
 import Tunniplaan from './pages/Tunniplaan'
 import { Contact } from './pages/Kontakt'
 
@@ -12,7 +12,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Avaleht />} />
-        <Route path="/teenused" element={<Teenused />} />
+        <Route path="/teenused" element={<Services />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/tunniplaan" element={<Tunniplaan />} />
         <Route path="/contact" element={<Contact />} />
