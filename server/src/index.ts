a2ac4c85b5014ad1489d56094ab6afa8b0b95d79
alisagudo <algudo@taltech.ee>
2025-11-21 03:02:49 +0200

    BACLEND

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { prisma } from "./db";
+import { prisma } from "./prisma";
 
 // Routerid:
 import roomsRouter from "./routes/rooms";
